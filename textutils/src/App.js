193c66740c353e77efca91f8f41dd5b0f1011c86
Seera Veera Pratap <veerapratapsirra@gmail.com>
2024-03-14 23:38:20 +0530

    First

---
+++ textutils/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'bootstrap';
+import Navbar from './components/Navbar';
+//import Blog from './components/Blog';
+import About from './components/About';
+
+function App() {
+  return (
+  <>
+<Navbar title="Enigma Entries"/>
+<div className="container my-2"></div>
+
+
+</>
+  );
+}
+
+export default App;
---
+++ textutils/src/components/Blog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function blog() {
+  return (
+    <div>
+      <div class="container">
+  content should be filled here!
+</div>
+    </div>
+  )
+}
