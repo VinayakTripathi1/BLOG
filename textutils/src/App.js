5aa81fbd1ba5b0b781bca47b71b22953e37a294e
Seera Veera Pratap <veerapratapsirra@gmail.com>
2024-03-15 00:04:06 +0530

    w

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -2,8 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import { Button } from 'bootstrap';
 import Navbar from './components/Navbar';
-//import Blog from './components/Blog';
-import About from './components/About';
 
 function App() {
   return (
