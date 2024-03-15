909886b18d76f38a65767966a1cbe29d5b1e0626
Vinayak Tripathi <tripathivinayak29@gmail.com>
2024-03-15 18:18:20 +0530

    Added a component Carousell in components and related images to public folder

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -2,13 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import { Button } from 'bootstrap';
 import Navbar from './components/Navbar';
+import Carousell from './components/Carousell';
 
 function App() {
   return (
   <>
 <Navbar title="Enigma Entries"/>
-<div className="container my-2"></div>
-
 
 </>
   );
---
+++ textutils/src/components/Carousell.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Carousell(){return (<div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+  
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img className="d-block w-100" src="Purpose-of-Blogging.jpeg" alt="First slide"/>
+    </div>
+    <div className="carousel-item">
+      <img className="d-block w-100" src="Purpose-of-Blogging.jpeg" alt="Second slide"/>
+    </div>
+    <div className="carousel-item">
+      <img className="d-block w-100" src="Purpose-of-Blogging.jpeg" alt="Third slide"/>
+    </div>
+  </div>
+</div>)}
